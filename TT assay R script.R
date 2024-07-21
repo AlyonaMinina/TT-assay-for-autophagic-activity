5715a88cbc858662c3bb5988a3a591b0b1b891b7
Alyona Minina <47862925+AlyonaMinina@users.noreply.github.com>
2024-07-21 21:01:57 +0200

    Update TT assay R script.R
    typos fix

--- TT assay R script.R
+++ TT assay R script.R
@@ -22,9 +22,9 @@ required_packages <- c("dplyr", "ggplot2", "stringr", "readr", "data.table", "pl
 lapply(required_packages, install_and_load)
 
 
-
 # Ask user to select the experiment directory
 Experiment_dir <- choose.dir(default = "", caption = "Select folder")
+
 
 # Create a timestamped subfolder
 timestamp <- format(Sys.time(), "%Y%m%d_%H%M%S")
@@ -39,16 +39,18 @@ csv_file_list <- list.files(path = Experiment_dir,
                             recursive = TRUE,
                             pattern = "\\.csv$",
                             full.names = TRUE) %>%
-  # Filter out files that are in directories named "Results_"
+ 
+# Filter out files that are in directories containing "R_analysis_" in the name
   .[!grepl("R_analysis_", dirname(.))]
+
 
 # Merge the .csv files and save the merged results
 merged_results <- csv_file_list %>%
   lapply(read_csv) %>%
-  bind_rows()
+  bind_rows
 
 # Replace spaces in the column names with underscores, add columns for the time point, organ, and genotype
-colnames(merged_results) <- gsub(" ", "_", colnames(merged_results))
+colnames(merged_results) <- gsub(" ","_", colnames(merged_results))
 merged_results <- merged_results %>%
   mutate(Genotype = if_else(str_detect(File_name, '823'), 'WT', 'KO'),
          Organ = if_else(str_detect(File_name, 'root'), 'Root', 'Shoot'),
@@ -76,81 +78,110 @@ KO_mean_data <- merged_results %>%
 normalized_data <- KO_mean_data %>%
   mutate(Normalized_ratio = Fluorescence_ratio_of_RFP_to_GFP / Mean_ratio_for_KO)
 
-# Write the normalized data to a CSV file in the subfolder
-write.csv(normalized_data, file.path(output_dir, "Merged_results.csv"))
+write.csv(normalized_data,file.path(output_dir, "Merged_results.csv"))
 
-# Run Two-way ANOVA and Tukey's HSD test on the normalized ratios for WT
+# Run Two-way-Anova and Tukey's HSD test on the normalized ratios for WT
 WT_only <- normalized_data %>%
-  filter(Genotype == "WT") %>%
+  filter(Genotype == "WT")
+
+# Log-transform the Normalized_ratio column for Anova test
+WT_only <- WT_only %>%
   mutate(Log_Normalized_ratio = log(Normalized_ratio))
 
-# Write the WT only data to a CSV file in the subfolder
-write.csv(WT_only, file.path(output_dir, "Merged_results_for_WT_only.csv"))
+# Save the modified dataframe as a CSV file
+write.csv(WT_only, file.path(output_dir,"Merged_results_for_WT_only.csv"))
 
 # Dynamically determine the levels for Time_point_h
 time_points <- sort(unique(as.numeric(normalized_data$Time_point_h)))
 normalized_data$Time_point_h <- factor(normalized_data$Time_point_h, levels = as.character(time_points))
 
-# Define the analysis function
-perform_analysis <- function(data, treatment, output_dir) {
-  # Filter the data by treatment
-  filtered_data <- data %>%
-    filter(Treatment == treatment)
+# Run comparisons for each treatment
+perform_analysis <- function(WT_only, trtm, output_dir) {
+ 
+   # Sort by treatment
+  filtered_data <- WT_only %>%
+    filter(Treatment == trtm)
   
-  # Run analysis of variance by Two-way ANOVA
+  
+  # Run analysis of variance by Two-way Anova
   Two_way_Anova <- aov(Log_Normalized_ratio ~ Time_point_h * Organ, data = filtered_data)
   
   # Run means comparison by Tukey's HSD test
   Tukey <- TukeyHSD(Two_way_Anova)
   
   # Add CLD column to the summary stats
-  cld <- multcompLetters4(Two_way_Anova, Tukey)
+  cld <- multcompLetters4(Two_way_Anova, Tukey)  
+  print(cld)
   
   # Create a table with factors and 3rd quantile
   Tukey_with_CLD <- filtered_data %>%
     group_by(Time_point_h, Organ) %>%
-    summarise(mean = mean(Log_Normalized_ratio), quant = quantile(Log_Normalized_ratio, probs = 0.75)) %>%
+    summarise(mean = mean(Log_Normalized_ratio), quant = quantile(Log_Normalized_ratio, probs = 0.75), .groups = 'drop') %>%
     arrange(desc(mean))
   
-  # Add CLD to the Tukey table
+  # Add CLD to the Tk table
   cld_df <- as.data.frame.list(cld$`Time_point_h:Organ`)
   Tukey_with_CLD$cld <- cld_df$Letters
+  write.csv(Tukey_with_CLD, file.path(output_dir,  paste0("Tukey_with_CLD_", trtm, ".csv")))
   
-  # Write the Tukey table with CLD to a CSV file in the subfolder
-  write.csv(Tukey_with_CLD, file.path(output_dir, paste0("Tukey_with_CLD_", treatment, ".csv")))
   
   # Build box plots for WT ratios
-  p1<-ggplot(WT_only, aes(Time_point_h, Fluorescence_ratio_of_RFP_to_GFP, fill = Organ)) +
+  # filter normalized data by treatment
+  normalized_filtered_data <- normalized_data %>%
+    filter(Treatment == trtm)
+  
+  # Sort organ categories
+  normalized_filtered_data$Organ <- factor(normalized_filtered_data$Organ, c("Shoot", "Root"))
+  
+  # Ensure CLDs are sorted the same way as the organ categories
+  Tukey_with_CLD$Organ <- factor(Tukey_with_CLD$Organ, levels = c("Shoot", "Root"))
+  
+  # Ensure that time points are taken in numerical order
+  normalized_filtered_data$Time_point_h <- factor(normalized_filtered_data$Time_point_h, c("0", "2", "16", "24", "36", "48"))
+  
+  # Ensure only WT ratios are plotted
+  filtered_data <- normalized_filtered_data %>%
+    filter(Genotype == "WT")
+  
+  # Build a box plot on WT ratios
+  p1<-ggplot(filtered_data , aes(Time_point_h, Fluorescence_ratio_of_RFP_to_GFP, fill = Organ)) +
     geom_boxplot(width = 0.5, size = 0.15, outlier.shape = 1, outlier.size = 1) +
     geom_text(data = Tukey_with_CLD, aes(x = Time_point_h, y = 3, label = cld), size = 3, vjust = -10, hjust = 0.5, position = position_dodge(width = 0.5)) +
     theme_bw() +
-    theme(panel.grid.major = element_line(size = 0.1), panel.grid.minor = element_line(size = 0.1)) +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
     scale_fill_manual(values = c("#83D40F", "#EA9A16")) +
-    labs(title = paste("WT, RFP/GFP ratio -", treatment), x = "Elapsed time (h)", y = "RFP/GFP ratio")
-  ggsave(file.path(output_dir, paste0("Box_plot_ratio_", treatment, ".pdf")), width = 5, height = 4)
- 
-   # Print the plot to RStudio’s Plots pane
+    labs(title = "WT, RFP/GFP ratio", x = "Elapsed time (h)", y = "RFP/GFP ratio")
+  ggsave(file.path(output_dir,paste0("Box_plot_ratio_", trtm, ".pdf")), width = 5, height = 4)
+  # Print the plot to RStudio’s Plots pane
   print(p1)
   
-  # Build box plot normalized WT ratios
-  p2<-ggplot(WT_only, aes(Time_point_h, Normalized_ratio, fill = Organ)) +
+  
+  # Build a box plot normalized WT ratios
+  p2<-ggplot(filtered_data , aes(Time_point_h, Normalized_ratio, fill = Organ)) +
     geom_boxplot(width = 0.5, size = 0.15, outlier.shape = 1, outlier.size = 1) +
-    theme_bw() +
+    theme_bw() + 
     geom_text(data = Tukey_with_CLD, aes(x = Time_point_h, y = 2, label = cld), size = 3, vjust = -20, hjust = 0.5, position = position_dodge(width = 0.5)) +
-    theme(panel.grid.major = element_line(size = 0.1), panel.grid.minor = element_line(size = 0.1)) +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
     scale_fill_manual(values = c("#83D40F", "#EA9A16")) +
-    labs(title = paste("WT, RFP/GFP ratio normalized to KO -", treatment), x = "Elapsed time (h)", y = "Normalized RFP/GFP ratio")
-  ggsave(file.path(output_dir, paste0("Box_plot_normalized_ratio_", treatment, ".pdf")), width = 5, height = 4)
+    labs(title = "WT, RFP/GFP ratio normalized to KO", x = "Elapsed time (h)", y = "Normalized RFP/GFP ratio")
+  ggsave(file.path(output_dir,paste0("Box_plot_normalized_ratio_", trtm, ".pdf")), width = 5, height = 4)
   # Print the plot to RStudio’s Plots pane
   print(p2)
+  
 }
+
 
 # Get the unique treatments
 unique_treatments <- unique(WT_only$Treatment)
 
 # Perform analysis for each unique treatment
-for (treatment in unique_treatments) {
-  if (!is.na(treatment)) {
-    perform_analysis(WT_only, treatment, output_dir)
+for (trtm in unique_treatments) {
+  if (!is.na(trtm)) {
+    perform_analysis(WT_only, trtm, output_dir)
   }
 }
+
+# # Perform analysis for each treatment
+# perform_analysis(WT_only, "AZD",output_dir)
+# perform_analysis(WT_only, "no_C",output_dir)
+# #perform_analysis(WT_only, "no_N",output_dir)
