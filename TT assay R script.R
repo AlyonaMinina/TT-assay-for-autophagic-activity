faadb931131f0f0d210efd0c60f930c4b38b4cc2
Alyona Minina <minina.alyona@gmail.com>
2024-09-13 17:39:59 +0200

    Debug of the R script
    -function is looped through data_for_analysis now
    
    -CLDs are applied only to the normalized data charts and aligned to be 5% above the max y value
    
    -removed unnecessary lines

--- TT assay R script.R
+++ TT assay R script.R
@@ -5,6 +5,8 @@
 # -Files with names containing "mock" will be considered 0h time point
 # -Time point values are taken from the file names as a substring found between space and the letter "h"
 
+#Clear the work space
+rm(list=ls())
 
 # Install and load the packages
 
@@ -25,8 +27,7 @@ lapply(required_packages, install_and_load)
 # Ask user to select the experiment directory
 Experiment_dir <- choose.dir(default = "", caption = "Select folder")
 
-
-# Create a timestamped subfolder
+# Create a time-tamped subfolder
 timestamp <- format(Sys.time(), "%Y%m%d_%H%M%S")
 output_dir <- file.path(Experiment_dir, paste0("R_analysis_", timestamp))
 dir.create(output_dir)
@@ -40,9 +41,8 @@ csv_file_list <- list.files(path = Experiment_dir,
                             pattern = "\\.csv$",
                             full.names = TRUE) %>%
   
-  # Filter out files that are in directories containing "R_analysis_" in the name
+# Filter out files that are in directories containing "R_analysis_" in the name
   .[!grepl("R_analysis_", dirname(.))]
-
 
 # Merge the .csv files and save the merged results
 merged_results <- csv_file_list %>%
@@ -61,10 +61,8 @@ merged_results <- merged_results %>%
            str_detect(File_name, '-C') ~ 'no_C',
            str_detect(File_name, 'mock') ~ 'mock',
            TRUE ~ NA_character_))
-
 merged_results <- merged_results %>%
   mutate(Time_point_h = if_else(str_detect(File_name, 'mock'), '0', Time_point_h))
-
 
 # Calculate the mean value for KO for each time point, organ, and treatment
 KO_mean <- merged_results %>%
@@ -76,7 +74,6 @@ KO_mean <- aggregate(Fluorescence_ratio_of_RFP_to_GFP ~ Organ + Time_point_h + T
                      FUN = mean, na.rm = TRUE)
 KO_mean <- KO_mean %>%
   dplyr::rename(Mean_ratio_for_KO = Fluorescence_ratio_of_RFP_to_GFP)
-
 
 # Merge mean_KO data frame with the original data frame, organized based on "Organ", "Treatment" and "Time_point_h"
 KO_mean_data <- merged_results %>%
@@ -103,16 +100,11 @@ write.csv(WT_only, file.path(output_dir,"Merged_results_for_WT_only.csv"))
 time_points <- sort(unique(as.numeric(normalized_data$Time_point_h)))
 normalized_data$Time_point_h <- factor(normalized_data$Time_point_h, levels = as.character(time_points))
 
-# Run comparisons for each treatment
-perform_analysis <- function(WT_only, trtm, output_dir) {
-  
-  # Sort by treatment and Ensure that 'mock' is included in the data
-  filtered_data <- data_for_analysis %>%
-    filter(Treatment == trtm | Treatment == "mock")
-  
-  
+# Run comparisons for each treatment (mock is included into each run)
+perform_analysis <- function(data_for_analysis, trtm, output_dir) {
+ 
   # Run analysis of variance by Two-way Anova
-  Two_way_Anova <- aov(Log_Normalized_ratio ~ Time_point_h * Organ, data = filtered_data)
+  Two_way_Anova <- aov(Log_Normalized_ratio ~ Time_point_h * Organ, data = data_for_analysis)
   
   # Run means comparison by Tukey's HSD test
   Tukey <- TukeyHSD(Two_way_Anova)
@@ -122,7 +114,7 @@ perform_analysis <- function(WT_only, trtm, output_dir) {
   print(cld)
   
   # Create a table with factors and 3rd quantile
-  Tukey_with_CLD <- filtered_data %>%
+  Tukey_with_CLD <- data_for_analysis %>%
     group_by(Time_point_h, Organ) %>%
     dplyr::summarise(mean = mean(Log_Normalized_ratio), quant = quantile(Log_Normalized_ratio, probs = 0.75), .groups = 'drop') %>%
     arrange(desc(mean))
@@ -132,55 +124,48 @@ perform_analysis <- function(WT_only, trtm, output_dir) {
   Tukey_with_CLD$cld <- cld_df$Letters
   write.csv(Tukey_with_CLD, file.path(output_dir,  paste0("Tukey_with_CLD_", trtm, ".csv")))
   
-  
   # Build box plots for WT ratios
-  # filter normalized data by treatment
-  normalized_filtered_data <- normalized_data %>%
-    filter(Treatment == trtm | Treatment == "mock")
-  normalized_filtered_data <- as.data.frame(normalized_filtered_data)
-  
   
   # Sort organ categories
-  normalized_filtered_data$Organ <- factor(normalized_filtered_data$Organ, c("Shoot", "Root"))
+  data_for_analysis$Organ <- factor(data_for_analysis$Organ, c("Shoot", "Root"))
   
   # Ensure CLDs are sorted the same way as the organ categories
   Tukey_with_CLD$Organ <- factor(Tukey_with_CLD$Organ, levels = c("Shoot", "Root"))
   
-  
-  
-  # Ensure only WT ratios are plotted
-  filtered_data <- normalized_filtered_data %>%
-    filter(Genotype == "WT")
-  filtered_data <- as.data.frame(filtered_data)
-  
-  
   # Build a box plot on WT ratios
-  p1<-ggplot(filtered_data , aes(Time_point_h, Fluorescence_ratio_of_RFP_to_GFP, fill = Organ)) +
+  
+  #NB! CLDs were not calculated for this plot
+  p1<-ggplot(data_for_analysis, aes(Time_point_h, Fluorescence_ratio_of_RFP_to_GFP, fill = Organ)) +
     geom_boxplot(width = 0.5, size = 0.15, outlier.shape = 1, outlier.size = 1) +
-    geom_text(data = Tukey_with_CLD, aes(x = Time_point_h, y = 3, label = cld), size = 3, vjust = -10, hjust = 0.5, position = position_dodge(width = 0.5)) +
     theme_bw() +
     theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
     scale_fill_manual(values = c("#83D40F", "#EA9A16")) +
     scale_y_continuous(limits = c(0, NA))+
     labs(title = paste0("WT, RFP/GFP ratio, ", trtm), x = "Elapsed time (h)", y = "RFP/GFP ratio")
   ggsave(file.path(output_dir,paste0("Box_plot_ratio_", trtm, ".pdf")), width = 5, height = 4)
-  # Print the plot to RStudio’s Plots pane
+  # Print the plot to RStudio’s Plots panel
   print(p1)
   
-  
   # Build a box plot normalized WT ratios
-  p2<-ggplot(filtered_data , aes(Time_point_h, Normalized_ratio, fill = Organ)) +
+  
+  # Calculate the y-axis maximum for placing CLDs on the plot
+  y_max <- max(data_for_analysis$Normalized_ratio, na.rm = TRUE)
+
+  # Set a fixed y position for CLDs to be 5% above the y_max value
+  Tukey_with_CLD <- Tukey_with_CLD %>%
+    mutate(label_y_position = y_max*1.05)
+
+    p2<-ggplot(data_for_analysis, aes(Time_point_h, Normalized_ratio, fill = Organ)) +
     geom_boxplot(width = 0.5, size = 0.15, outlier.shape = 1, outlier.size = 1) +
     theme_bw() + 
-    geom_text(data = Tukey_with_CLD, aes(x = Time_point_h, y = 2, label = cld), size = 3, vjust = -20, hjust = 0.5, position = position_dodge(width = 0.5)) +
+    geom_text(data = Tukey_with_CLD, aes(x = Time_point_h, y = label_y_position, label = cld), size = 3, vjust = 0, hjust = 0.5, position = position_dodge(width = 0.5)) +
     theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
     scale_fill_manual(values = c("#83D40F", "#EA9A16")) +
     scale_y_continuous(limits = c(0, NA))+
     labs(title = paste0("WT, RFP/GFP ratio normalized to KO, ", trtm), x = "Elapsed time (h)", y = "Normalized RFP/GFP ratio")
   ggsave(file.path(output_dir,paste0("Box_plot_normalized_ratio_", trtm, ".pdf")), width = 5, height = 4)
-  # Print the plot to RStudio’s Plots pane
+  # Print the plot to RStudio’s Plots panel
   print(p2)
-  
 }
 
 # Get the unique treatments, excluding 'mock'
